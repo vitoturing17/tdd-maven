d775e3c389cd6dd7d8deca0f1ab99e7b11c2eac9
Thimoty Barbieri <thimoty@thimoty.it>
2017-06-04 08:44:39 +0200

    aded parameterized

--- src/test/java/it/corso/calculator/CalculatorImplTest.java
+++ src/test/java/it/corso/calculator/CalculatorImplTest.java
@@ -2,12 +2,51 @@ package it.corso.calculator;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+/**
+ * 
+ * @author thimo
+ *
+ */
+@RunWith(Parameterized.class)
 public class CalculatorImplTest {
 
+	/**
+	 * Parametrized unit tests
+	 * 1) Create Fields
+	 * 2) Create Constructor for Injection of Parameters
+	 * 3) Create static method to load a Fixture and annotate as @Parameters
+	 * 4) Pass parameters to test method
+	 * 5) Declare the test runner as Parametrized
+	 */
+	private int num1, num2, expected;
+	
+	public CalculatorImplTest(int num1, int num2, int expected) {
+		this.num1 = num1;
+		this.num2 = num2;
+		this.expected = expected;
+	}
+	
+	@Parameters
+	public static Collection<Integer[]> getFixtureData() {
+		
+		return Arrays.asList(new Integer[][] {
+			{-1, 2, 1},
+			{2, 2, 4},
+			{-5, -5, -10}
+		});
+		
+	}
+	
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -15,8 +54,8 @@ public class CalculatorImplTest {
 	@Test
 	public void addShouldReturnAResult() {
 		Calculator c = new CalculatorImpl();
-		int result = c.add(10, 20);
-		assertEquals(30, result);
+		int result = c.add(num1, num2);
+		assertEquals(expected, result);
 	}
 
 }
